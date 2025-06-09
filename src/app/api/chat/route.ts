32efa1ba7fbb6be2e29a56aa45cc66e949bd24d2
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-09 11:51:58 -0400

    basic openai chat interface, including stop & regenerate handling. tested basic tooling with weather example

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,48 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText, tool } from 'ai';
+import type { Message } from 'ai';
+import { z } from 'zod';
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: Message[] } = await req.json() as { messages: Message[] };
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    messages,
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location (fahrenheit)',
+        parameters: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+    },
+  });
+
+  return result.toDataStreamResponse({
+    getErrorMessage: error => {
+      if (error == null) {
+        return 'unknown error';
+      }
+
+      if (typeof error === 'string') {
+        return error;
+      }
+
+      if (error instanceof Error) {
+        return error.message;
+      }
+
+      return JSON.stringify(error);
+    },
+  });
+}
