5a44d1440e61fc7e18b05bd77533e86d5aa05d48
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-12 18:58:01 -0400

    FOR DEPLOY

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
     baseURL: process.env.NODE_ENV === "production"
-        ? "https://your-domain.com" // Replace with your production URL
+        ? "https://t3-chat-app-ho6w.vercel.app/" // Replace with your production URL
         : "http://localhost:3000"
 });
 
