ce069c2b1a532ced0ceebd1705a3a57090b2c350
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-12 19:16:13 -0400

    production check is trolling

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NODE_ENV === "production"
-        ? "https://t3-chat-app-ho6w.vercel.app/" // Replace with your production URL
-        : "http://localhost:3000"
+    baseURL: "https://t3-chat-app-ho6w.vercel.app/" // Replace with your production URL
 });
 
 export const {
