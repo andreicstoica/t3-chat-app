b588684b6ba10c5a4e2874c67bdd96ea16eaf93b
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-12 17:29:30 -0400

    updated images, sign in/out forms and flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { cookies } from "next/headers";
+import Link from "next/link";
 import Home from "~/components/Home";
+import { Logout } from "~/components/logout";
 
-export default function Page() {
-  return <Home />;
+export default async function Page() {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("better-auth.session_token");
+
+  return (
+    <div className="flex justify-end">
+      {sessionCookie ? <Logout /> : <Link href="/signin">Login</Link>}
+    </div>
+  );
+
+  //return <Home />;
 }
