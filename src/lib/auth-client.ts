c91f619f6ffb594d2c2b887b86962f9135e722bd
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-07-24 12:52:03 -0400

    forgot to switch auth url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    //baseURL: "https://t3-chat-app-ho6w.vercel.app/" // Replace with your production URL
-    baseURL:'http://localhost:3000'
+    baseURL: "https://t3-chat-app-ho6w.vercel.app/" // Replace with your production URL
+    //baseURL:'http://localhost:3000'
 });
 
 export const {
